4a4efc8428549b4d7a6e05e8242a9d8d4bf13ed7
adamvaughn <adamdvaughn@gmail.com>
2022-05-20 21:58:45 -0500

    info sections - placeholder text

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import Video from '../../video/video.mp4';
+import { Button } from '../ButtonElements';
+import {
+    HeroContainer,
+    HeroBg,
+    VideoBg,
+    HeroContent,
+    HeroH1,
+    HeroP,
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight
+} from './HeroElements';
+
+const HeroSection = () => {
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+  return (
+    <HeroContainer>
+        <HeroBg>
+            <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+        </HeroBg>
+        <HeroContent>
+            <HeroH1>Video Dev</HeroH1>
+            <HeroP>
+                Sign up today!
+            </HeroP>
+            <HeroBtnWrapper>
+                <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'>
+                    Get Started! {hover ? <ArrowForward /> : <ArrowRight />}
+                </Button>
+            </HeroBtnWrapper>
+        </HeroContent>
+    </HeroContainer>
+  )
+}
+
+export default HeroSection
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'VideoDev',
+    headLine: 'Job Search and Video Conferencing',
+    description: 'We help link up developers with clients to find the perfect match to create your project!',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/vid-conf.svg'),
+    alt: 'img',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'VideoDev',
+    headLine: 'Job Search and Video Conferencing',
+    description: 'We help link up developers with clients to find the perfect match to create your project!',
+    buttonLabel: 'Get Started',
+    imgStart: true,
+    img: require('../../images/conference.svg'),
+    alt: 'img',
+    dark: true,
+    primary: true,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'services',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'VideoDev',
+    headLine: 'Job Search and Video Conferencing',
+    description: 'We help link up developers with clients to find the perfect match to create your project!',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/vid-conf.svg'),
+    alt: 'img',
+    dark: true,
+    primary: true,
+    darkText: false
+}
