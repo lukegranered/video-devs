cd0a3b2b20952e69860d5d8518996587c3b9ab50
adamvaughn <adamdvaughn@gmail.com>
2022-05-23 19:24:48 -0500

    fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import Profile from "./pages/profile/Profile";
-import Login from "./components/login/login";
+//import Profile from "./pages/profile/Profile";
+//import Login from "./components/login/login";
 import React from 'react';
-import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages';
 import SigninPage from './pages/signin';
