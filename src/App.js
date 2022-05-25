ca9db92521c300f31a8150400045796779f744de
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-05-25 04:55:48 -0500

    changes install apollo/client jwt and concurrently adding entries into json try to connect the backend with frontend

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './pages/index';
-import SigninPage from './pages/signin';
-import SignupPage from './pages/signup';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/index";
+import SigninPage from "./pages/signin";
+import SignupPage from "./pages/signup";
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} exact />
-        <Route path="/signin" element={<SigninPage />} exact />
-        <Route path="/signup" element={<SignupPage />} exact />
-      </Routes>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/signin" element={<SigninPage />} exact />
+          <Route path="/signup" element={<SignupPage />} exact />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
