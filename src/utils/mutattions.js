209dc3b81b3a022d48b9461d365ce15d76f92ea1
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-05-25 04:58:09 -0500

    try to connect backend with frontend, adding files auth for token and mutations

---
+++ src/utils/mutattions.js
@@ -0,0 +1,26 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
