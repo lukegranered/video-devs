1124e9e74154a62a2b038b6b6e12b62e3c776579
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-05-25 05:27:40 -0500

    adding the sigin and signup connect to apollo backend correct mutations file name - connected sign/singup backend and frontend

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,38 +1,92 @@
-import React from 'react'
+import React, { useState } from "react";
 import {
-    Container,
-    FormWrap,
-    Icon,
-    FormContent,
-    Form,
-    FormH1,
-    FormLabel,
-    FormInput,
-    FormButton,
-    Text
-} from './SigninElements';
+  Container,
+  FormWrap,
+  Icon,
+  FormContent,
+  Form,
+  FormH1,
+  FormLabel,
+  FormInput,
+  FormButton,
+  Text,
+} from "./SigninElements";
+
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../../utils/mutations";
+import Auth from "../../utils/auth";
 
 const SignIn = () => {
+  const [login, { error }] = useMutation(LOGIN_USER);
+  const [formState, setFormState] = useState({ email: "", password: "" });
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async event => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      console.log(data);
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+    // clear form values
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <>
-        <Container>
-            <FormWrap>
-                <Icon to='/'>VideoDev</Icon>
-                <FormContent>
-                    <Form action='#'>
-                        <FormH1>Sign in to your account</FormH1>
-                        <FormLabel htmlFor='for'>Email</FormLabel>
-                        <FormInput type='email' required />
-                        <FormLabel htmlFor='for'>Password</FormLabel>
-                        <FormInput type='password' required />
-                        <FormButton type='submit'>Continue</FormButton>
-                        <Text>Forgot password?</Text>
-                    </Form>
-                </FormContent>
-            </FormWrap>
-        </Container>
+      <Container>
+        <FormWrap>
+          <Icon to="/">VideoDev</Icon>
+          <FormContent>
+            <Form onSubmit={handleFormSubmit}>
+              <FormH1>Sign in to your account</FormH1>
+              <FormLabel htmlFor="for">Email</FormLabel>
+              <FormInput
+                placeholder="Your email"
+                name="email"
+                type="email"
+                id="email"
+                required
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <FormLabel htmlFor="for">Password</FormLabel>
+              <FormInput
+                type="password"
+                required
+                className="form-input"
+                placeholder="******"
+                name="password"
+                id="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <FormButton type="submit">Continue</FormButton>
+              <Text>Forgot password?{error && <div>Signin failed</div>}</Text>
+            </Form>
+          </FormContent>
+        </FormWrap>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
